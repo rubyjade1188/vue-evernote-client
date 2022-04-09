d01e3a12e0e44793cdc077a50eb8101a0524f557
sallyzhou <sallymjz1188@gmail.com>
2022-04-10 01:14:05 +0800

    run build

--- src/components/NoteBookList.vue
+++ src/components/NoteBookList.vue
@@ -4,6 +4,7 @@
     <ul>
       <li><router-link to="/note/1">笔记本1</router-link></li>
       <li><router-link to="/note/2">笔记本2</router-link></li>
+      <li><router-link to="/note/3">笔记本3</router-link></li>
     </ul>
   </div>
 </template>
