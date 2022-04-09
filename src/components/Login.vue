88636720edf6ff684846f3b7103fe44d1b7b3af9
sallyzhou <sallymjz1188@gmail.com>
2022-04-10 01:01:41 +0800

    update routes

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      msg: "This is login page"
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
