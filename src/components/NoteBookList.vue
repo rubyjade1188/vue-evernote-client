e92c13454e0b140f7b33f295e6e282b9676c3d96
sallyzhou <sallymjz1188@gmail.com>
2022-04-28 21:58:32 +0800

    use message box components of element ui

--- src/components/NoteBookList.vue
+++ src/components/NoteBookList.vue
@@ -60,47 +60,119 @@ export default {
   },
   methods: {
     createNoteBook() {
-      let title = window.prompt("新建笔记本");
-      if (title.trim === "") {
-        alert("笔记本名字不能为空");
-        return;
-      }
-      Notebooks.addBook({ title }).then(res => {
-        console.log(res);
-        alert(res.msg);
-        res.data.friendlyUpdatedAt = friendlyDate(res.data.updatedAt);
-        this.notebooks.unshift(res.data);
-      });
+      this.$prompt("请输入笔记本标题", "新建笔记本", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        inputPattern: /^.{1,30}$/,
+        inputErrorMessage: "标题不能为空，且不超过30个字符"
+      })
+        .then(({ value }) => {
+          return Notebooks.addBook({ title: value });
+        })
+        .then(res => {
+          console.log(res);
+          res.data.friendlyUpdatedAt = friendlyDate(res.data.updatedAt);
+          this.notebooks.unshift(res.data);
+          this.$message({
+            type: "success",
+            message: res.msg
+          });
+        })
+        .catch(err => {
+          // 这里可能是点了取消按钮，此时err为cancel
+          // console.log(err)
+          if (err === "cancel") {
+            this.$message({
+              type: "info",
+              message: "取消输入"
+            });
+          } else {
+            // 当接收到后端返回的err时
+            this.$message({
+              type: "error",
+              message: err.msg
+            });
+          }
+        });
     },
     editNoteBook(notebook) {
-      console.log("editing...", notebook);
-      let title = window.prompt("修改标题", notebook.title);
-      Notebooks.updateBook(notebook.id, { title }).then(res => {
-        console.log(res);
-        alert(res.msg);
-        // 之前是发送请求给后端，后端修改好数据后，显示修改成功，
-        // 前端如果不刷新，不能重新渲染得到改变后的title
-        // 那么直接在前端修改
-        // 即使再刷新，得到的是一致的数据（修改好的），逻辑自洽
-        notebook.title = title;
-        // console.log("test", notebook.friendlyUpdatedAt);
-        notebook.friendlyUpdatedAt = friendlyDate(Date.now());
+      let title = "";
+      this.$prompt("请输入新标题", "修改笔记本", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        inputPattern: /^.{1,30}$/,
+        inputErrorMessage: "标题不能为空，且不超过30个字符"
+      })
+        .then(({ value }) => {
+          title = value;
+          return Notebooks.updateBook(notebook.id, { title });
+        })
+        .then(res => {
+          console.log(res);
+          this.$message({
+            type: "success",
+            message: res.msg
+          });
+          // 之前是发送请求给后端，后端修改好数据后，显示修改成功，
+          // 前端如果不刷新，不能重新渲染得到改变后的title
+          // 那么直接在前端修改
+          // 即使再刷新，得到的是一致的数据（修改好的），逻辑自洽
+          notebook.title = title;
+          // console.log("test", notebook.friendlyUpdatedAt);
+          notebook.friendlyUpdatedAt = friendlyDate(Date.now());
 
-        // 因为我们是按照updatedAt排序，所以edit后的数组也要变为第一个
-        this.notebooks.splice(this.notebooks.indexOf(notebook), 1);
-        this.notebooks.unshift(notebook);
-      });
+          // 因为我们是按照updatedAt排序，所以edit后的数组也要变为第一个
+          this.notebooks.splice(this.notebooks.indexOf(notebook), 1);
+          this.notebooks.unshift(notebook);
+        })
+        .catch(err => {
+          // 这里可能是点了取消按钮，此时err为cancel
+          // console.log(err)
+          if (err === "cancel") {
+            this.$message({
+              type: "info",
+              message: "取消输入"
+            });
+          } else {
+            // 当接收到后端返回的err时
+            this.$message({
+              type: "error",
+              message: err.msg
+            });
+          }
+        });
     },
     deleteNoteBook(notebook) {
-      console.log("deleting...", notebook);
-      let isConfirm = window.confirm("确定要删除这个笔记本吗？");
-      if (isConfirm) {
-        Notebooks.deleteBook(notebook.id).then(res => {
+      this.$confirm("确定要删除这个笔记本吗？", "删除笔记本", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          return Notebooks.deleteBook(notebook.id);
+        })
+        .then(res => {
           console.log(res);
           this.notebooks.splice(this.notebooks.indexOf(notebook), 1);
-          alert(res.msg);
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+        })
+        .catch(res => {
+          // console.log(res);
+          if (res === "cancel") {
+            this.$message({
+              type: "info",
+              message: "已取消删除"
+            });
+          } else {
+            this.$message({
+              type: "error",
+              message: res.msg
+            });
+          }
         });
-      }
     }
   }
 };
